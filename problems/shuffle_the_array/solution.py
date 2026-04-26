b328468b0d6b8164b23b4149b772491bbce6c289
이건우 <nameless767304@gmail.com>
2026-04-26 13:34:14 +0000

    Sync LeetCode submission - Shuffle the Array (python3)

---
+++ problems/shuffle_the_array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        length = len(nums)
+        for idx in range(length // 2):
+            ans.append(nums[idx])
+            ans.append(nums[idx + length // 2])
+
+        return ans
+
