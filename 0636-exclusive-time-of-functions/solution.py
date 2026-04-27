533f72a9f4802fc0b9452e396051e970641756e4
이건우 <nameless767304@gmail.com>
2026-04-27 01:27:07 +0000

    [Update: LeetCode Solutions] Runtime - 3 ms (94.59%), Memory - 19.4 MB (22.70%)

---
+++ 0636-exclusive-time-of-functions/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        ans = [0 for _ in range(n)]
+        stack = []
+        prev_time = 0
+        
+        for log in logs:
+            id, status, timestamp = log.split(":")
+            id, timestamp = int(id), int(timestamp)
+
+            if status == "start":
+                if stack:
+                    ans[stack[-1]] += timestamp - prev_time
+
+                stack.append(id)
+                prev_time = timestamp
+
+            else:
+                ans[stack.pop()] += timestamp - prev_time + 1
+                prev_time = timestamp + 1
+
+        return ans
+                
+                
+
+
+        
+
+
